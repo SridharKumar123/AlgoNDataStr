493b226345a7d046b55873ac5da2008d045cfcf8
SridharKumar123 <sridhar.phoenix2007@gmail.com>
2020-09-13 22:11:11 +0530

    Update programs.java

--- recursion/programs.java
+++ recursion/programs.java
@@ -108,9 +108,83 @@ private void perm(int index, List<Integer> input, List<List<Integer>> output) {
                                                                             4.  Subsets
 **************************************************************************************************************************************************************************
 
+// Given an input array [1,2,3] find the powerset of it - [[], [1], [2], [1, 2], [3], [1, 3], [2, 3], [1, 2, 3]]
+// we start with [], then match it with 1 and we get [],[1]. we match this with 2 to get [],[1],[2],[1,2] and so on
+// during the second iteration, we add in the same outer list, so we get its size before addinng new elements
+
+private static void subsets(List<Integer> inputs, List<List<Integer>> op) {
+
+		for(int i=0; i<inputs.size();i++) {
+			int size = op.size();
+			for(int j=0; j<size;j++) {
+				List<Integer> temp = new ArrayList<>(op.get(j));
+				temp.add(inputs.get(i));
+				op.add(temp);
+			}			
+		}		
+	}
 
 **************************************************************************************************************************************************************************
                                                                             5.  Interleaving Strings
 **************************************************************************************************************************************************************************
+//Given three strings A, B and C. checks whether C is an interleaving of A and B.
+//C is said to be interleaving A and B, if it contains all characters of A and B and order of all characters in individual strings is preserved
+//"aaa", "aaf", "aafaaa" - here we can form the third using first 2 without changing order.
+// catch here is, if we start in first string and move on, once we reach the 3rd element, we see that it does not match.
+// now we move one step back in this and one step forward in second element and check, if it matches we continue, else we move another step back in first 
+// this process of back and forth is a costly operation as we might end up trying the same position again to see if it matches.
+// first we have bruteforce approach
+
+private static boolean interStrings(String first, String second, String main) {
+		
+		return interStrings(first, second, main,0,0,0);		
+	}
+	
+private static boolean interStrings(String first, 
+			String second, String main, int i, int j, int k) {
+		if(i==first.length() && j==second.length())
+			return true;
+		
+		if(k< main.length() && i < first.length() && main.charAt(k)==first.charAt(i)) {
+			if(interStrings(first,second,main,i+1,j,k+1)) {
+				return true;
+			}
+		}
+		if(k< main.length() && j < second.length() && main.charAt(k)==second.charAt(j)) {
+			return interStrings(first, second, main,i,j+1,k+1);
+		}
+		
+		return false;	
+	}
+
+// for the same above solution, we apply memoization to make sure we donot travel to the same faulty position again
+
+private static boolean interStringMemoization(String first, String second, String main) {
+		
+		Boolean [][] cache = new Boolean[first.length()][second.length()];
+		return interStrings(first, second, main,0,0,0, cache);		
+	}
+	
+private static boolean interStrings(String first, 
+			String second, String main, int i, int j, int k, Boolean[][] cache) {
+		if(i==first.length() && j==second.length())
+			return true;
+		
+		if(cache[i][j]!=null)
+			return cache[i][j];
+		
+		if(k< main.length() && i < first.length() && main.charAt(k)==first.charAt(i)) {
+			cache[i][j] = interStrings(first,second,main,i+1,j,k+1);
+			if(cache[i][j]) {
+				return true;
+			}
+		}
+		if(k< main.length() && j < second.length() && main.charAt(k)==second.charAt(j)) {
+			cache[i][j] = interStrings(first, second, main,i,j+1,k+1);
+			return cache[i][j];
+		}
+		
+		return false;	
+	}
 
 
