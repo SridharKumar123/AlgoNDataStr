7ae4e59f88e6dae2755095e91279c7d1b8988bc3
SridharKumar123 <sridhar.phoenix2007@gmail.com>
2020-09-20 23:09:39 +0530

    Update programs.java

--- dfs/programs.java
+++ dfs/programs.java
@@ -542,41 +542,399 @@ boolean isBalanced = true;
 
 
 **************************************************************************************************************************************************************************
-                                                                            3.  Symmetric Tree
+                                                                            10.  Minimum Depth of Binary Tree
 **************************************************************************************************************************************************************************
+/*Given a binary tree, find its minimum depth.
+The minimum depth is the number of nodes along the shortest path from the root node down to the nearest leaf node.
+Note: A leaf is a node with no children.
+Example:
+Given binary tree [3,9,20,null,null,15,7],
+    3
+   / \
+  9  20
+    /  \
+   15   7
+return its minimum depth = 2.*/
 
-
+public int minDepth(TreeNode root) {
+        if(root==null)
+            return 0;
+        //return height(root);
+        return heightBFS(root);
+    }
+    
+    private static int heightBFS(TreeNode root){
+        Queue<TreeNode> que = new LinkedList<>();
+        que.offer(root);
+        int length = 0;
+        while(!que.isEmpty()){
+            int size = que.size();            
+            for(int i=0; i<size; i++){
+                TreeNode node = que.poll();
+                if(node.left==null && node.right==null){
+                    que.clear();
+                    break;
+                }
+                if(node.left!=null)
+                    que.offer(node.left);
+                if(node.right!=null)
+                    que.offer(node.right);
+            }
+            length++;
+        }
+        return length;
+    }
+    
+    private static int height(TreeNode root){
+        if(root==null)
+            return 0;
+        int left = height(root.left);
+        int right = height(root.right);
+        if((left!=0 && right!=0) || (left==0 && right==0))
+            return Math.min(left,right)+1;
+        else if(left!=0)
+            return left+1;
+        else 
+            return right+1;
+    }
 
 
 **************************************************************************************************************************************************************************
-                                                                            3.  Symmetric Tree
+                                                                           11.  Path Sum
 **************************************************************************************************************************************************************************
+/*Given a binary tree and a sum, determine if the tree has a root-to-leaf path such that adding up all the values along the path equals the given sum.
+Note: A leaf is a node with no children.
+Example:
+Given the below binary tree and sum = 22,
+      5
+     / \
+    4   8
+   /   / \
+  11  13  4
+ /  \      \
+7    2      1
+return true, as there exist a root-to-leaf path 5->4->11->2 which sum is 22.*/
 
+// The basic idea is to subtract the value of current node from sum until it reaches a leaf node and the subtraction equals 0, then we know that we got a hit.
+//Otherwise the subtraction at the end could not be 0.
 
+private boolean hasPathHelper(TreeNode root, int sum){        
+        if(root==null)
+            return false;
+            // we calculate to see if the leaf matches the remaining
+        if(root.left == null && root.right==null && root.val-sum ==0)
+            return true;
+        boolean left = hasPathHelper(root.left, sum-root.val);
+        boolean right = hasPathHelper(root.right, sum-root.val);
+        return left || right;
+    } 
+        
+// why two stack ? path stack keep record of visited nodes in a DFS way, sub stack keep record of sum till current node. 
+//Only check ==sum when current node doesn't have any child, otherwise keep adding its child node. 
+//The visited nodes are no longer in the stack, so it won't be double checked for the same node
+    private boolean hasPathSumHelpIterative(TreeNode root, int sum){
+        Deque<TreeNode> stack = new ArrayDeque<>();
+        Deque<Integer> values = new ArrayDeque<>();
+        stack.push(root);
+        values.push(sum);
+        while(!stack.isEmpty()){
+            // modified from inorder stack traversal
+            int val = values.pop();
+            TreeNode currentNode = stack.pop();
+            if(currentNode.left==null && currentNode.right==null && val==currentNode.val)
+                return true;
+            if(currentNode.left!=null){
+                stack.push(currentNode.left);
+                values.push(val-currentNode.val);
+            }
+            if(currentNode.right!=null){
+                stack.push(currentNode.right);
+                values.push(val-currentNode.val);
+            }
+        }
+        return false;
+    }
 
 **************************************************************************************************************************************************************************
-                                                                            3.  Symmetric Tree
+                                                                            12.  Path Sum II
 **************************************************************************************************************************************************************************
+/*Given a binary tree and a sum, find all root-to-leaf paths where each path's sum equals the given sum.
+Note: A leaf is a node with no children.
+Example:
+Given the below binary tree and sum = 22,
+      5
+     / \
+    4   8
+   /   / \
+  11  13  4
+ /  \    / \
+7    2  5   1
+Return:
+[
+   [5,4,11,2],
+   [5,8,4,5]
+]*/
 
+public List<List<Integer>> pathSum(TreeNode root, int sum) {
+        List<List<Integer>> output = new ArrayList<>();
+        if(root==null)
+            return output;
+        pathSumHelper(root,sum,new ArrayList<>(),output);
+        return output;
+    }
+    
+    private void pathSumHelper(TreeNode root, int sum,
+                List<Integer> currentList, List<List<Integer>> output ){
+        if(root==null)
+            return ;
+        
+        List<Integer> tempList = new ArrayList<>(currentList);
+        tempList.add(root.val);
+        if(root.left==null && root.right==null && root.val-sum==0){
+            output.add(tempList);          
+        }
+        pathSumHelper(root.left,sum-root.val,tempList,output);
+        pathSumHelper(root.right,sum-root.val,tempList,output);
+    }
 
 **************************************************************************************************************************************************************************
-                                                                            3.  Symmetric Tree
+                                                                            13.  Flatten Binary Tree to Linked List
 **************************************************************************************************************************************************************************
+/*Given a binary tree, flatten it to a linked list in-place.
+For example, given the following tree:
+    1
+   / \
+  2   5
+ / \   \
+3   4   6
+The flattened tree should look like:
+1
+ \
+  2
+   \
+    3
+     \
+      4
+       \
+        5
+         \
+          6*/
+    
+// if lest is not null, 
+// take right out
+// move left to right
+// move right to last of left
 
-
+  private void flattenHelper(TreeNode root){
+        if(root==null)
+            return;
+        if(root.left!=null){
+            TreeNode right = root.right;
+            root.right = root.left;
+            root.left = null;
+            TreeNode temp = root;
+            while(temp.right!=null){
+                temp = temp.right;
+            }
+            temp.right = right;
+        }
+        root = root.right;
+        flattenHelper(root);
+    }
 
 **************************************************************************************************************************************************************************
-                                                                            3.  Symmetric Tree
+                                                                            14.  Populating Next Right Pointers in Each Node
 **************************************************************************************************************************************************************************
+/*You are given a perfect binary tree where all leaves are on the same level, and every parent has two children. The binary tree has the following definition:
+struct Node {
+  int val;
+  Node *left;
+  Node *right;
+  Node *next;
+}
+Populate each next pointer to point to its next right node. If there is no next right node, the next pointer should be set to NULL.
+Initially, all next pointers are set to NULL.
+ 
+Follow up:
+You may only use constant extra space.
+Recursive approach is fine, you may assume implicit stack space does not count as extra space for this problem.
+ 
+Example 1:
+Input: root = [1,2,3,4,5,6,7]
+Output: [1,#,2,3,#,4,5,6,7,#]
+Explanation: Given the above perfect binary tree (Figure A), your function should populate each next pointer to point to its next right node, just like in Figure B. The serialized output is in level order as connected by the next pointers, with '#' signifying the end of each level.
+ 
+Constraints:
+The number of nodes in the given tree is less than 4096.
+-1000 <= node.val <= 1000  */
 
-
+ // BFS should easily do this
+public Node connect(Node root) {
+        if(root==null)
+            return null;
+        Queue<Node> que = new LinkedList<>();
+        que.offer(root);
+        while(!que.isEmpty()){
+            int size = que.size();            
+            for(int i=0; i< size; i++){
+                Node node = que.poll();               
+                if(i== size-1){
+                    node.next = null;
+                }else{
+                    node.next = que.peek();
+                }
+                if(node.left!=null)
+                    que.offer(node.left);
+                if(node.right!=null)
+                    que.offer(node.right);
+            }
+        }
+        return root;
+    }
 
 **************************************************************************************************************************************************************************
-                                                                            3.  Symmetric Tree
+                                                                            15.  Populating Next Right Pointers in Each Node - II
 **************************************************************************************************************************************************************************
-
-
+/*Given a binary tree
+struct Node {
+  int val;
+  Node *left;
+  Node *right;
+  Node *next;
+}
+Populate each next pointer to point to its next right node. If there is no next right node, the next pointer should be set to NULL.
+Initially, all next pointers are set to NULL.
+ 
+Follow up:
+You may only use constant extra space.
+Recursive approach is fine, you may assume implicit stack space does not count as extra space for this problem.
+ 
+Example 1:
+Input: root = [1,2,3,4,5,null,7]
+Output: [1,#,2,3,#,4,5,7,#]
+Explanation: Given the above binary tree (Figure A), your function should populate each next pointer to point to its next right node, just like in Figure B.
+The serialized output is in level order as connected by the next pointers, with '#' signifying the end of each level.
+ 
+Constraints:
+The number of nodes in the given tree is less than 6000.
+-100 <= node.val <= 100*/       
+public Node connect(Node root) {
+        if(root==null)
+            return null;
+        Queue<Node> que = new LinkedList<>();
+        que.offer(root);
+        while(!que.isEmpty()){
+            int size = que.size();            
+            for(int i=0; i< size; i++){
+                Node node = que.poll();               
+                if(i== size-1){
+                    node.next = null;
+                }else{
+                    node.next = que.peek();
+                }
+                if(node.left!=null)
+                    que.offer(node.left);
+                if(node.right!=null)
+                    que.offer(node.right);
+            }
+        }
+        return root; 
+    }
 
 **************************************************************************************************************************************************************************
-                                                                            3.  Symmetric Tree
+                                                                            16.  Sum Root to Leaf Numbers
 **************************************************************************************************************************************************************************
+/*Given a binary tree containing digits from 0-9 only, each root-to-leaf path could represent a number.
+An example is the root-to-leaf path 1->2->3 which represents the number 123.
+Find the total sum of all root-to-leaf numbers.
+Note: A leaf is a node with no children.
+Example:
+Input: [1,2,3]
+    1
+   / \
+  2   3
+Output: 25
+Explanation:
+The root-to-leaf path 1->2 represents the number 12.
+The root-to-leaf path 1->3 represents the number 13.
+Therefore, sum = 12 + 13 = 25.
+Example 2:
+Input: [4,9,0,5,1]
+    4
+   / \
+  9   0
+ / \
+5   1
+Output: 1026
+Explanation:
+The root-to-leaf path 4->9->5 represents the number 495.
+The root-to-leaf path 4->9->1 represents the number 491.
+The root-to-leaf path 4->0 represents the number 40.
+Therefore, sum = 495 + 491 + 40 = 1026.*/
+
+public int sumNumbers(TreeNode root) {
+        if(root==null)
+            return 0;
+        List<String> output = new ArrayList<>();
+        sumNumbersHelper(root, new StringBuilder(), output);
+        int sum = 0;
+        for(int i=0; i<output.size(); i++){
+            if(output.get(i).length()>0){
+            Integer val = Integer.valueOf(output.get(i));
+            sum += val;
+            }
+        }
+        return sum;
+    }
+    
+    private void sumNumbersHelper(TreeNode root,
+                        StringBuilder builder, List<String> output){
+        if(root==null)
+            return ;
+        StringBuilder tempBuilder = new StringBuilder(builder);
+        tempBuilder.append(root.val);
+        if(root.left==null && root.right==null){            
+            output.add(tempBuilder.toString());           
+        }
+        sumNumbersHelper(root.left,tempBuilder,output);
+        sumNumbersHelper(root.right,tempBuilder,output);
+        
+    }
+
+Binary Tree Right Side View
+/*Given a binary tree, imagine yourself standing on the right side of it, return the values of the nodes you can see ordered from top to bottom.
+
+Example:
+
+Input: [1,2,3,null,5,null,4]
+Output: [1, 3, 4]
+Explanation:
+
+   1            <---
+ /   \
+2     3         <---
+ \     \
+  5     4       <---
+*/
+
+public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> output = new ArrayList<>();
+        if(root==null)
+            return output;
+        
+        Queue<TreeNode> que = new LinkedList<>();
+        que.offer(root);
+        while(!que.isEmpty()){
+            int size = que.size();
+            for(int i=0; i<size; i++){
+                if(i==size-1)
+                    output.add(que.peek().val);
+                TreeNode node = que.poll();
+                if(node.left!=null)
+                    que.offer(node.left);
+                if(node.right!=null)
+                    que.offer(node.right);
+            }
+        }
+        return output;
+    }
