9ddf2561153bc699d7d0f28f31713ace17c3625d
SridharKumar123 <sridhar.phoenix2007@gmail.com>
2020-09-25 00:06:20 +0530

    Update programs.java

--- graph/programs.java
+++ graph/programs.java
@@ -154,6 +154,28 @@ class Solution {
     }
 }
 
+**************************************************************************************************************************************************************************
+                                                                            3. Number of Islands 2 
+**************************************************************************************************************************************************************************
+You have a map that marks the location of a treasure island. Some of the map area has jagged rocks and dangerous reefs. Other areas are safe to sail in. 
+There are other explorers trying to find the treasure. So you must figure out a shortest route to the treasure island.
+
+Assume the map area is a two dimensional grid, represented by a matrix of characters. You must start from the top-left corner of the map and can move one block up,
+down, left or right at a time. The treasure island is marked as X in a block of the matrix. X will not be at the top-left corner. 
+Any block with dangerous rocks or reefs will be marked as D. You must not enter dangerous blocks. You cannot leave the map area. Other areas O are safe to sail in. 
+The top-left corner is always safe. Output the minimum number of steps to get to the treasure.
+
+Example:
+
+Input:
+[['O', 'O', 'O', 'O'],
+ ['D', 'O', 'D', 'O'],
+ ['O', 'O', 'O', 'O'],
+ ['X', 'D', 'D', 'O']]
+
+Output: 5
+Explanation: Route is (0, 0), (0, 1), (1, 1), (2, 1), (2, 0), (3, 0) The minimum route takes 5 steps.
+
 
 **************************************************************************************************************************************************************************
                                                                             3. Find Bridges
@@ -296,7 +318,9 @@ Time Complexity : O(V+E)
                                                                             3. Find Articulation points
 **************************************************************************************************************************************************************************
   
-You are given an undirected connected graph. An articulation point (or cut vertex) is defined as a vertex which, when removed along with associated edges, makes the graph disconnected (or more precisely, increases the number of connected components in the graph). The task is to find all articulation points in the given graph.
+You are given an undirected connected graph. An articulation point (or cut vertex) is defined as a vertex which, when removed along with associated edges,
+makes the graph disconnected (or more precisely, increases the number of connected components in the graph). 
+The task is to find all articulation points in the given graph.
 
 Input:
 The input to the function/method consists of three arguments:
