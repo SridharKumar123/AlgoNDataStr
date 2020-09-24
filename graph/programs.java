e3211030f48c1b960da79b6a1872885bb9caf1eb
SridharKumar123 <sridhar.phoenix2007@gmail.com>
2020-09-25 00:38:54 +0530

    Update programs.java

--- graph/programs.java
+++ graph/programs.java
@@ -176,6 +176,57 @@ Input:
 Output: 5
 Explanation: Route is (0, 0), (0, 1), (1, 1), (2, 1), (2, 0), (3, 0) The minimum route takes 5 steps.
 
+Similar question:
+Problem Statement: Find the shortest path to reach the destination in the matrix (Amazon Delivery Coding Challenge)
+
+Amazon Fresh is a grocery delivery service that offers consumers the option of purchasing their groceries online and having them delivered on schedule. 
+The Amazon Fres team is planning a route for a delivery truck to deliver customer orders in the city of Techland. The planner will create a delivery area for 
+each order to effectively plan the route. The area is abstracted as a grid.
+Not all locations are accessible by road. The truck only needs to make a single delivery.
+Write an algorithm to determine the minimum distance required for the truck to deliver the order.
+
+Assumptions
+Some places in the delivery area cannot be accessed by the driver as there are no roads in those locations.
+The delivery area can be represented as a two-dimensional grid of integers where each integer represents one cell.
+The truck must start from the top-left corner of the area which is always accessible, and can move one cell up, down., left or right at a time.
+Tne trucks must navigate around the area without roads and cannot leave the area.
+The accessible areas are represented as 1, an area with without roads is represented by 0 and the destination is represented by 9.
+Input
+
+The input to the function/method consists of three arguments numRows, an integer representing the number of rows, numColumns,
+an integer representing the number of columns, area, representing the two-dimensional grid of integers.
+
+Output
+
+Return an integer representing the total distance traversed to deliver the order; else return -1
+
+Constraints
+
+1<= numRows, numColumns<=1000
+
+Example
+Input
+numRows=3
+numColumns=3
+area
+[[1, 0, 0]
+[1, 0, 0]
+[1, 9, 1]]
+Output
+3
+Exmpainaltion
+
+Starting from the top-left corner, the truck traversed the cell
+
+(0, 0)-> (1,0)->(2,0)->(2,1)
+The truck traversed the total distance 3 to deliver the order, so the output is 3.
+
+
+
+Can be solved in BFS and DFS. BFS is much better in time complexity.
+
+https://www.youtube.com/watch?v=KiCBXu4P-2Y
+
 
 **************************************************************************************************************************************************************************
                                                                             3. Find Bridges
