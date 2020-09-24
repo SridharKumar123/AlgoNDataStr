ffb42b61e1a71b4c5fa590475ca0c504c62bc01b
SridharKumar123 <sridhar.phoenix2007@gmail.com>
2020-09-24 19:54:22 +0530

    Update programs.java

--- graph/programs.java
+++ graph/programs.java
@@ -224,6 +224,31 @@ Time Complexity : O(V+E)
                                                                             3. Find Articulation points
 **************************************************************************************************************************************************************************
   
+You are given an undirected connected graph. An articulation point (or cut vertex) is defined as a vertex which, when removed along with associated edges, makes the graph disconnected (or more precisely, increases the number of connected components in the graph). The task is to find all articulation points in the given graph.
+
+Input:
+The input to the function/method consists of three arguments:
+
+numNodes, an integer representing the number of nodes in the graph.
+numEdges, an integer representing the number of edges in the graph.
+edges, the list of pair of integers - A, B representing an edge between the nodes A and B.
+Output:
+Return a list of integers representing the critical nodes.
+
+Example:
+	       4
+	      /
+	     3
+	    / \
+	   1   2
+	   \  / \
+	     0   5
+	        / 
+               6 
+Input: numNodes = 7, numEdges = 7, edges = [[0, 1], [0, 2], [1, 3], [2, 3], [2, 5], [5, 6], [3, 4]]
+Output: [2, 3, 5]
+
+Articulation Algorithm:
 1) With bridge : On a connected component with 3 or more vertices, if there is a edge(u,v) which is a bridge, either u or v is an articulation point.
 2) Without bridge: cycle indicates that there is an articulation point. in previous algo, if (id(e.from) == lowlink(e.to)) - this condition is for cycle.
      - the above condition of cycle fails only for STARTING NODE, if it has just 0 or 1 outgoing edge. 
