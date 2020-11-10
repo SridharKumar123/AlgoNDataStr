de3e8e0163f1d34353259303862b08f83d7f1d5c
SridharKumar123 <sridhar.phoenix2007@gmail.com>
2020-11-10 07:19:55 +0530

    Update prog.java

--- string/prog.java
+++ string/prog.java
@@ -3,3 +3,49 @@ Ceasar cipher encryptor
 given a string and an int k , return a new string by shifting every letter by k positions in alphabet order
 xyz - k=2
 
+integer can be very huge, in that case we need to use modulo
+unicode value - it takes in char and return its unicode value
+lets say the range of unicode for a--z is 97 -- 122.
+now if we just take unicode + k, it will not work for use case like z. 
+as z value is 122. ==> 122+2 = 124 - this is not unicode for b. we have gone out of range.
+
+lets say nLc as new Letter count,
+1) we ensure that key is a value within 26. if key is large number our approach will result in val grt than 122
+2) if nLc is less than 122, then we know we can directly find the char
+3) if greater than 122, we need to take the modulo and add it to 96. so its like we start from beginning and find the char
+
+key = key % 26
+nLc = int(letter) + key
+if(nLc <=122)
+  return (char) nLc
+else 
+  return (char) (96 + nLc%122)  
+
+time : O(N)
+space : O(N) - op store  
+
+
+ import java.util.*;
+
+class Program {
+  public static String caesarCypherEncryptor(String str, int key) {
+    key = key % 26;
+		//97   122
+		StringBuilder builder = new StringBuilder();
+		for(int i=0; i<str.length();i++){
+			char current = str.charAt(i);
+			int val = (int) current;
+			int newCount = val + key;
+			if(newCount <= 122){
+				char charVal = (char)newCount;
+				builder.append(String.valueOf(charVal));
+			}else{
+				int reminder = newCount - 122;
+				int newVal = 96 + reminder;
+				char charVal = (char)newVal;
+				builder.append(String.valueOf(charVal));
+			}
+		}
+		return builder.toString();
+  }
+}
